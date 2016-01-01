14503ae223ef1302697e894957b7973b7528953d
Michael Colon <michaelcolon@youaredreaminginspacewheretimeisjustenergy.local>
2015-12-31 20:10:18 -0500

    huffman index.

--- huffman_index.js
+++ huffman_index.js
@@ -4,20 +4,50 @@
   var b = require('./huffman');
   var tree = require('./BinaryHeap');
 
+  var replacer = function(text) {
+    var obj = Object.create(replacer.prototype);
+    obj.text = text;
+    // recursive replace all in string.
+    obj.replaceAll = function(replace, replacement) {
+      if ( this.text.indexOf(replace) >= 0) {
+        this.text = this.text.replace(replace,replacement);
+        this.replaceAll(replace, replacement);
+      }
+    };
+    // recursive replace all hash keys with values.
+    obj.recursiveReplace = function(hash, keys) {
+      if (keys.length > 0) {
+        key = keys.pop();
+        this.replaceAll(key, hash[key]);
+        this.recursiveReplace(hash,keys);
+      }
+    };
+    return obj;
+  };
+
+  var text = 'once a pon a time in thug land.';
   console.log('\n');
-  console.log('Huffman coding for characters in text.')
+  console.log('Huffman coding for words in text.');
   var code = a.HuffmanCodeWords(0);
-  var wordhash = code.frequencyWordHash('once a pon a time in thug land.'.replace('.',' .'));
+  var wordhash = code.frequencyWordHash(text.replace('.',' .'));
   var wordTree = code.buildTree(wordhash);
   code.encode(wordTree[1], '');
-  console.log(code.encoding);
+  encoding = code.encoding;
+  keys = Object.keys(encoding);
+  var replacer = replacer(text);
+  replacer.recursiveReplace(encoding, keys);
+  console.log(replacer.text);
 
   console.log('\n');
-  console.log('Huffman coding for words in text.')
+  console.log('Huffman coding for characters in text.');
   var code = b.HuffmanCode(0);
-  var hash = code.frequencyCharacterHash('once a pon a time in thug land.');
+  var hash = code.frequencyCharacterHash(text);
   var tree = code.buildTree(hash);
   code.encode(tree[1], '');
-  console.log(code.encoding);
+  encoding = code.encoding;
+  keys = Object.keys(encoding);
+  replacer.text = text;
+  replacer.recursiveReplace(encoding, keys);
+  console.log(replacer.text);
 
 }());
